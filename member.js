74ad8476224a00ce32e2a6a46773dee4cec50cb0
Ivan Windon <99699684+iwindon@users.noreply.github.com>
2024-11-25 17:24:50 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,6 @@
+function skillsMember() {
+  return {
+    skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Node.js', 'MongoDB', 'Express'],
+    bio: 'I am a full stack web developer with a passion for learning and growing.'
+  }
+}
